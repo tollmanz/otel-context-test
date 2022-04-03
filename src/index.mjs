6c9a0d245425dced253de745bb2343f78a534806
Zack Tollman <tollmanz@gmail.com>
2022-04-02 22:23:53 -0500

    Fully working with sdk only

--- src/index.mjs
+++ src/index.mjs
@@ -1,11 +1,11 @@
 import "./tracing.mjs";
 
-import opentelemetry from '@opentelemetry/api';
+import opentelemetry from "@opentelemetry/api";
 import Fastify from "fastify";
 
 const waitFor = (durMs) => new Promise((resolve) => setTimeout(resolve, durMs));
 
-const tracer = opentelemetry.trace.getTracer("context-override-app");
+const tracer = opentelemetry.trace.getTracer("context-override-demo", "1.0.0");
 
 const server = Fastify({});
 
--- src/tracing.mjs
+++ src/tracing.mjs
@@ -1,45 +1,25 @@
 import process from "process";
 import { NodeSDK, tracing } from "@opentelemetry/sdk-node";
 import { Resource } from "@opentelemetry/resources";
-import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
-import { BasicTracerProvider, ConsoleSpanExporter, SimpleSpanProcessor } from '@opentelemetry/sdk-trace-base';
+import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
 import { HttpInstrumentation } from "@opentelemetry/instrumentation-http";
 
-import { W3CTraceContextPropagator2 } from './W3CTraceContextPropagator.js';
+import { W3CTraceContextPropagator2 } from "./W3CTraceContextPropagator.js";
 
-const traceExporterConsole = new tracing.ConsoleSpanExporter();
-
-// The service name is REQUIRED! It is a resource attribute, which means that it will be present on all observability data that your service generates.
 const sdk = new NodeSDK({
+  // The service name is REQUIRED! It is a resource attribute, which means that it will be present on all observability data that your service generates.
   resource: new Resource({
-    [SemanticResourceAttributes.SERVICE_NAME]: 'zack-test',
+    [SemanticResourceAttributes.SERVICE_NAME]: "zack-test",
   }),
-  traceExporter: traceExporterConsole,
+
+  // The SDK will automatically create a new trace processor for you, but you can also create your own.
+  traceExporter: new tracing.ConsoleSpanExporter(),
 
   // Instrumentations allow you to add auto-instrumentation packages
   instrumentations: [new HttpInstrumentation()],
-});
 
-const provider = new BasicTracerProvider({
-  resource: new Resource({
-    [SemanticResourceAttributes.SERVICE_NAME]: 'zack-test-2',
-  }),
-});
-
-// Configure span processor to send spans to the exporter
-provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
-
-/**
- * Initialize the OpenTelemetry APIs to use the BasicTracerProvider bindings.
- *
- * This registers the tracer provider with the OpenTelemetry API as the global
- * tracer provider. This means when you call API methods like
- * `opentelemetry.trace.getTracer`, they will use this tracer provider. If you
- * do not register a global tracer provider, instrumentation which calls these
- * methods will receive no-op implementations.
- */
-provider.register({
-  propagator: new W3CTraceContextPropagator2(),
+  // Propagators allow you to add custom propagation packages
+  textMapPropagator: new W3CTraceContextPropagator2(),
 });
 
 sdk
